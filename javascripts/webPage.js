8f1e0c976bf2ac2f1fd0826b75113a55b2c6ca1b
narvaezv <narvaezv94@gmail.com>
2016-05-04 13:44:27 -0500

    Added files via upload

---
+++ javascripts/webPage.js
@@ -0,0 +1,15 @@
+var selectElement = document.getElementById('selector');
+var divElement = document.getElementById('output');
+
+selectElement.onchange = function () {
+    var selectedValue = selectElement.options[selectElement.selectedIndex].value;
+    if (selectedValue == 'empleado') {
+        var id = '<input type="text" name="id">';
+        var form = '<form>First name:<br>' + id + '<br></form>';
+        divElement.innerHTML = '<p> </p><p>Has seleccionado empleado.</p>' + form;
+    } else if (selectedValue == 'saab') {
+        divElement.innerHTML = '<p> </p><p>saab2</p>';
+    } else if (selectedValue == 'mercedes') {
+        divElement.innerHTML = '<p> </p><p>mercedes</p>';
+    }
+};
