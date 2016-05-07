f648feb699d1a8eacb08650e83cc589bbbcb87b0
narvaezv <narvaezv94@gmail.com>
2016-05-07 15:01:51 -0500

    V1

--- php/insertEmpleado.php
+++ php/insertEmpleado.php
@@ -1,11 +1,11 @@
 <html>
 <body>
 <?php
-$id = $_POST['id'];
-$nombre = $_POST['nombre'];
-$apellido = $_POST['apellido'];
-$telefono = $_POST['telefono'];
-$tipo = $_POST['tipo'];
+$id = $_POST["id"];
+$nombre = $_POST["nombre"];
+$apellido = $_POST["apellido"];
+$telefono = $_POST["telefono"];
+$tipo = $_POST["tipo"];
 echo "<p>Your name is $nombre y tu id es $id<p/>";
 ?>
 </body>
