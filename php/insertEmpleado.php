5167a2e627c87459956d50d265918fd2af370b67
narvaezv <narvaezv94@gmail.com>
2016-05-07 14:51:50 -0500

    Errores

--- php/insertEmpleado.php
+++ php/insertEmpleado.php
@@ -4,6 +4,6 @@ $nombre = $POST_['nombre'];
 $apellido = $POST_['apellido'];
 $telefono = $POST_['telefono'];
 $tipo = $POST_['tipo'];
-echo "Your name is $nombre y tu id es $id";
+echo "<p>Your name is $nombre y tu id es $id<p/>";
 
 ?>
